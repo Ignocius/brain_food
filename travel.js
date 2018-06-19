4902b1bdb546797e0409ce806899e899ecac493e
HOME-Ricsi <szabo.ricsi@zip2000.hu>
2018-06-19 19:26:47 +0200

    first steps files created travel algorithm started

---
+++ travel.js
@@ -0,0 +1,36 @@
+let optimizeDestination = function (destination) {    
+            
+    let destTye = ''
+
+    Object.values(destination).forEach((route) => {
+        if (route) {
+            destTye = 'relyedDest'
+        } else {
+            destTye ='normalDest'
+        }                
+    })
+    
+    switch(destTye) {
+        case 'normalDest':
+            console.log('single')
+            break
+        case 'relyedDest':
+            console.log('rely')
+            break
+        default:
+            console.log('Unrecognized type');
+    }
+}
+
+let objX = {
+    x : '',
+    z: 'y',
+    y: 'z',
+}
+let y = {
+    x : '',
+}
+console.log(objX)
+
+optimizeDestination(objX)
+optimizeDestination(y)
